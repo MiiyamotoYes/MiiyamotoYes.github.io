dca1a5433b64ca469e81443c874ca30552829803
MiiyamotoYes <60615397+MiiyamotoYes@users.noreply.github.com>
2020-03-04 13:23:02 -0500

    Add files via upload

---
+++ scripts/Scripts.js
@@ -0,0 +1,18 @@
+function changeWords() {
+    document.getElementById("foot").innerHTML = "Contact me: &nbsp; Phone: (804) 335-4735 &nbsp;  Email: nathanwassem@gmail.com";
+    document.getElementById("foot").style.transitionDuration = "5s";
+
+
+}
+
+function changeBack() {
+
+    document.getElementById("foot").innerHTML = "^ Contact me ^ ";
+
+}
+
+function Madlib() {
+    document.getElementById("madlib").innerHTML = "This is my Mad Lib game that I made for my computer science course. We were tasked with creating a mad lib game in python, however I went the extra mile to include not one but 3 unique mad lib games. Click me to try";
+    
+
+}
